5ab3c8a10a83b6006b68a9e2552db130617a3cea
Ibrahim Yassin <iay5015@psu.edu>
2023-10-14 00:36:00 -0400

    This is the completed GUI game. But it won't open the window.

--- PictureMemoryGame.java
+++ PictureMemoryGame.java
@@ -11,7 +11,7 @@ public class PictureMemoryGame extends JFrame {
     private ArrayList<String> cardImages;
     private JButton[] cardButtons;
     private int numberOfMatches;
-    private int firstCardIndex;
+    private int firstCardIndex = -1;
     private int secondCardIndex;
 
     public PictureMemoryGame() {
@@ -48,7 +48,7 @@ public class PictureMemoryGame extends JFrame {
         for (int i = 0; i < cardButtons.length; i++) {
             final int index = i;
             cardButtons[i] = new JButton();
-            cardButtons[i].setIcon(new ImageIcon("illuminati.png"));
+            cardButtons[i].setIcon(new ImageIcon("Illuminati.png"));
             cardButtons[i].addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -78,14 +78,48 @@ public class PictureMemoryGame extends JFrame {
 
     private void handleCardClick (int index) {
         if (cardButtons[index].getIcon() == null) {
-        return;
+            return; //Already matched card, do nothing.
         }
+
         if (firstCardIndex == -1) {
             firstCardIndex = index;
             cardButtons[firstCardIndex].setIcon(new ImageIcon(imagePaths.get(index)));
-        } else{
+        } else {
             secondCardIndex = index;
             cardButtons[secondCardIndex].setIcon(new ImageIcon(imagePaths.get(index)));
+
+            Timer timer = new Timer(1000, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (imagePaths.get(firstCardIndex).equals(imagePaths.get(secondCardIndex))) {
+                        cardButtons[firstCardIndex].setIcon(null);
+                        cardButtons[secondCardIndex].setIcon(null);
+                        cardImages.set(firstCardIndex, null);
+                        cardImages.set(secondCardIndex, null);
+                        numberOfMatches++;
+
+                        if (numberOfMatches == imagePaths.size() / 2) {
+                            JOptionPane.showMessageDialog(null, "Congratulations! You've won the game, now go do something productive with your life!");
+                            System.exit(0);
+                        }
+                    } else {
+                        cardButtons[firstCardIndex].setIcon(new ImageIcon("Illuminati.png"));
+                        cardButtons[secondCardIndex].setIcon(new ImageIcon("Illuminati.png"));
+                    }
+                    firstCardIndex = -1;
+                }
+            });
+            timer.setRepeats(false);
+            timer.start();
         }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
     }
     }
